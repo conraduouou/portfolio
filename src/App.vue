7a3d687fa62a5ced6d1628f414fa81432e34b8fd
Louise Lalu <louiserafaellalu@gmail.com>
2023-08-21 21:23:42 +0800

    create footer component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from './views/components/HeaderComponent.vue'
+import Footer from './views/components/FooterComponent.vue'
 import HomePage from './views/pages/Home/HomePage.vue'
 </script>
 
 <template>
   <Header/>
   <HomePage/>
+  <Footer/>
 </template>
---
+++ src/views/components/FooterComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+    </div>
+</template>
+
+<style scoped>
+div {
+    display: flex;
+    gap: max(8vw, 2em);
+    justify-content: center;
+    align-items: center;
+    padding: min(70px, 15vh) 0;
+}
+
+a {
+    text-decoration: none;
+    color: var(--color-neutral-600);
+    font-weight: 500;
+}
+</style>
