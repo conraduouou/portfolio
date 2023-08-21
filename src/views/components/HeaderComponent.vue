cf56a4a172e23b346229a81e4775ccb5fba43d0e
Louise Lalu <louiserafaellalu@gmail.com>
2023-08-21 14:38:58 +0800

    remove background color of header and apply filter

--- src/views/components/HeaderComponent.vue
+++ src/views/components/HeaderComponent.vue
@@ -52,7 +52,7 @@ header>div:nth-child(1) {
   align-items: center;
   width: 100%;
   padding: 4vh 6vw 2.5vh 6vw;
-  background: var(--color-background);
+  backdrop-filter: blur(2em);
   box-shadow: 0 4px 8px 0 rgba(200, 200, 200, 0.2);
 }
 
