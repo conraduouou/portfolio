1efe8ac65bdc9c5aa17ddc9e44ff1d6211d41903
Louise Lalu <louiserafaellalu@gmail.com>
2024-02-28 19:19:16 +0800

    improve vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,1 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
-};
+export const publicPath = process.env.NODE_ENV === "production" ? "/portfolio/" : "/";
