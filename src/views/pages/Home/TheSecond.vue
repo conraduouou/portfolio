7fe2216ffe1d39411566da74336fa1c63ec35176
Louise Lalu <louiserafaellalu@gmail.com>
2024-02-28 21:35:36 +0800

    support images in github pages by creating a dynamic variable

--- src/views/pages/Home/TheSecond.vue
+++ src/views/pages/Home/TheSecond.vue
@@ -1,32 +1,37 @@
 <script setup>
 import WorkItem from '../../components/WorkItem.vue'
+import { computed } from 'vue'
+
+const basePath = computed(() => {
+  return window.location.origin
+})
 </script>
 
 <template>
   <section>
     <h2>Some of my works</h2>
     <div class="work-items">
-      <WorkItem class="work-item" :is-mobile=true :asset-url="'/images/bigbrew.png'">
+      <WorkItem class="work-item" :is-mobile=true :asset-url="`${basePath}/images/bigbrew.png`">
         This was my first project using Flutter, done for the BigBrew milktea franchise.
       </WorkItem>
-      <WorkItem class="work-item" :is-mobile=true :asset-url="'/images/nft.png'">
+      <WorkItem class="work-item" :is-mobile=true :asset-url="`${basePath}/images/nft.png`">
         This was a side hustle offered to me by my co-worker, done in two (2) weeks!
       </WorkItem>
-      <WorkItem class="work-item" :is-mobile=true :asset-url="'/images/toolmart.png'">
+      <WorkItem class="work-item" :is-mobile=true :asset-url="`${basePath}/images/toolmart.png`">
         This was another of my projects in my 2nd year as well, exploring more performance
         optimizations and animations in Flutter. I also learned how to create an API using
         ASP.NET Core and MongoDB.
       </WorkItem>
-      <WorkItem class="work-item" :is-mobile=true :asset-url="'/images/coffee.png'">
+      <WorkItem class="work-item" :is-mobile=true :asset-url="`${basePath}/images/coffee.png`">
         This was my project for my software engineering subject, which was also done using
         Flutter. By this time, I was exploring more state management options as well as
         implementing API locally using xAMPP.
       </WorkItem>
-      <WorkItem class="work-item" :asset-url="'/images/hospital.png'">
+      <WorkItem class="work-item" :asset-url="`${basePath}/images/hospital.png`">
         This was one of my projects in my 2nd year in college which allowed me to be
         proficient in state as well as architecture management in Flutter.
       </WorkItem>
-      <WorkItem class="work-item" :asset-url="'/images/diligens.png'">
+      <WorkItem class="work-item" :asset-url="`${basePath}/images/diligens.png`">
         This was my first major web development project that pushed me to learn HTML, CSS,
         and JS in-depth. I was also able to implement a simple backend solution for the
         site using PHP.
